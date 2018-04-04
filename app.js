d13e0d80b0c7937279d26be85c28eb2d63954d85
AmilaIndika789 <amilaindika789@gmail.com>
2018-04-04 13:15:32 +0530

    Added basic routes to handle GET requests coming to host/waterQuality

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');     //Import the express package
 const app = express();      //Create an express application
 
+//Import route file needed to forward the requests coming to 'host/waterQuality'
+const productRoutes = require('./api/routes/waterQuality');
+
 //Setting up a middle-ware to pass the incoming requests
-app.use((req, res, next) => {
-    //Send a json response with HTTP status 200
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+app.use('/waterQuality', productRoutes);
 
 module.exports = app;
