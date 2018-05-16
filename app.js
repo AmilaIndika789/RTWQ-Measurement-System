a48e832f251a2026e37f925c56bd38edfe734887
AmilaIndika789 <amilaindika789@gmail.com>
2018-05-16 21:51:19 +0530

    Added few more functionalities    Automatic reloading    Default login    Basic error handling

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');     //Import the express package
 const app = express();      //Create an express application
+const morgan = require('morgan');   //Import morgan package
 
 //Import route file needed to forward the requests coming to 'host/waterQualities'
 const waterQualitiesRoutes = require('./api/routes/waterQualities');
 const endNodesRoutes = require('./api/routes/endNodes');
 
 //Setting up a middle-ware to pass the incoming requests
-app.use('/waterQualities', waterQualitiesRoutes);
-app.use('/endNodes', endNodesRoutes);
+    //Log all the requests by passing through the morgan middleware
+    app.use(morgan('dev'));
 
-module.exports = app;
+    //Routes which should handle requests
+    app.use('/waterQualities', waterQualitiesRoutes);
+    app.use('/endNodes', endNodesRoutes);
+
+    //Handle the errors that does not valid to any available routes
+    app.use((req, res, next) => {
+        const error = new Error('Not found');   //Create a error object
+        error.status = 404;  //Return status code of 404
+        next(error);    //Forward the request with the error
+    });
+
+    //Handle the errors that are thrown from anywhere else from the application.    e.g.: Database errors
+    app.use((error, req, res, next)=>{
+        res.status(error .status || 500);
+        res.json({
+            error: {
+                message: error.message
+            }
+        });
+    });
+
+    module.exports = app;
